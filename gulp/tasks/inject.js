ce667aa8aa7fc74201f7446612914350a693c7c7
ryandeussing <ryan@ryandeussing.com>
2017-01-17 16:38:55 -0500

    no color numbrs

--- gulp/tasks/inject.js
+++ gulp/tasks/inject.js
@@ -6,12 +6,14 @@ const inject = require('gulp-inject');
 gulp.task('inject:head', () =>
   gulp.src('.tmp/src/_includes/head.html')
     .pipe(inject(gulp.src('.tmp/assets/stylesheets/*.css'), {ignorePath: '.tmp'}))
+    .pipe(inject(gulp.src('.tmp/assets/javascript/*.js'), {ignorePath: '.tmp'}))
     .pipe(gulp.dest('.tmp/src/_includes'))
 );
 
+
 // 'gulp inject:footer' -- injects our index.js file into the end of our HTML
-gulp.task('inject:footer', () =>
-  gulp.src('.tmp/src/_layouts/default.html')
-    .pipe(inject(gulp.src('.tmp/assets/javascript/*.js'), {ignorePath: '.tmp'}))
-    .pipe(gulp.dest('.tmp/src/_layouts'))
-);
+// gulp.task('inject:footer', () =>
+//   gulp.src('.tmp/src/_layouts/default.html')
+//     .pipe(inject(gulp.src('.tmp/assets/javascript/*.js'), {ignorePath: '.tmp'}))
+//     .pipe(gulp.dest('.tmp/src/_layouts'))
+// );
--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,1 +1,5 @@
-
+$(function() {
+  $.getJSON("http://gold-bugle.cloudvent.net/colors.json", function(data) {
+    console.log(JSON.stringify(data));
+  });
+});
