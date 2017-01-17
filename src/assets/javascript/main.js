af94e7054df11b03e479388105c9417178e7c7b4
ryandeussing <ryan@ryandeussing.com>
2017-01-17 16:56:34 -0500

    single pixel working

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,5 +1,19 @@
 $(function() {
-  $.getJSON("http://gold-bugle.cloudvent.net/colors.json", function(data) {
-    console.log(JSON.stringify(data));
+  $.getJSON("http://gold-bugle.cloudvent.net/colors.json", function(json) {
+    // console.log(JSON.stringify(data));
+    var pixel = $('.inner-square').first();
+    var color = json.colors[0].hex;
+    pixel.css("background-color", color);
+
   });
+
+
+  // var json = $.getJSON("http://gold-bugle.cloudvent.net/colors.json");
+  // var pixel = document.getElementsByClassName("square")[0];
+  // var color = json.colors[0].hex;
+  // pixel.css("background-color", color);
+
+
 });
+
+
