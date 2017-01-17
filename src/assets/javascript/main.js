31f481089c58d5f0642a3e79908084bb26fb6f62
ryandeussing <ryan@ryandeussing.com>
2017-01-17 17:49:42 -0500

    wip

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,19 +1,11 @@
-$(function() {
-  $.getJSON("http://gold-bugle.cloudvent.net/colors.json", function(json) {
-    // console.log(JSON.stringify(data));
-    var pixel = $('.inner-square').first();
-    var color = json.colors[0].hex;
-    pixel.css("background-color", color);
-
-  });
-
-
-  // var json = $.getJSON("http://gold-bugle.cloudvent.net/colors.json");
-  // var pixel = document.getElementsByClassName("square")[0];
-  // var color = json.colors[0].hex;
-  // pixel.css("background-color", color);
-
-
-});
+$(function () {
+  $.getJSON ('http://gold-bugle.cloudvent.net/colors.json', function (json) {
+    var colors = json.colors
+    var pixels = $('.inner-square')
+    pixels.each( function( i ){
+      $(this).css('background-color', colors[i].hex)
+    })
+  })
+})
 
 
