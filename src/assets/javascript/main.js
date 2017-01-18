f186c4bc3ad726c40d740a20525bb1ead245856b
ryandeussing <ryan@ryandeussing.com>
2017-01-17 19:41:39 -0500

    fadig in

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,9 +1,10 @@
 $(function () {
-  $.getJSON ('http://gold-bugle.cloudvent.net/colors.json', function (json) {
+  $.getJSON ('http://elegant-bean.cloudvent.net/colors.json', function (json) {
     var colors = json.colors
     var pixels = $('.inner-square')
+    var times = [100, 200, 300, 400, 500, 600, 700, 800, 900]
     pixels.each( function( i ){
-      $(this).css('background-color', colors[i].hex)
+      $(this).css('background-color', colors[i].hex).delay(times[Math.floor(Math.random() * times.length)]).fadeIn(500);
     })
   })
 })
