29ff7c1f8a44deeb8f5619c52395ab4c64ec6a8d
ryandeussing <ryan@ryandeussing.com>
2017-01-19 18:15:41 -0500

    Revert "repeating on ipadPro etc"
    This reverts commit 735ef0e64ca0844bae91f00de9120d9ba77856f8.

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,13 +1,8 @@
 $.getJSON ('http://elegant-bean.cloudvent.net/colors.json', function (json) {
   var colors = json.colors
-  var pixels = $('.regular > .inner-square')
-  var tallpixels = $('.tall > .inner-square')
+  var pixels = $('.inner-square')
   var times = [1000, 2500, 5000, 6000, 7000, 8000, 10000]
   pixels.each( function( i ){
-    $(this).append('<p class="mv0 ttu name black-75 absolute">' + colors[i].name + '<p>')
-    $(this).css('background-color', colors[i].hex).delay(times[Math.floor(Math.random() * times.length)]).fadeIn(500)
-  })
-  tallpixels.each( function( i ){
     $(this).append('<p class="mv0 ttu name black-75 absolute">' + colors[i].name + '<p>')
     $(this).css('background-color', colors[i].hex).delay(times[Math.floor(Math.random() * times.length)]).fadeIn(500)
   })
